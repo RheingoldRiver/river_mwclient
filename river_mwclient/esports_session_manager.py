6c071a38b7ca6e35cde6da450326b29e3d17ffa3
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2020-03-02 00:37:22 -0600

    only allow one username to be logged in

--- river_mwclient/esports_session_manager.py
+++ river_mwclient/esports_session_manager.py
@@ -9,18 +9,18 @@ class EsportsSessionManager(object):
     """Manages instances of EsportsClient
     """
     existing_wikis = {}
-    credentials: AuthCredentials = None
-    login_args = {}
 
     def get_client(self, wiki: str = None, client: WikiClient = None,
-                 credentials: AuthCredentials = None, stg: bool = False,
-                 **kwargs):
+                   credentials: AuthCredentials = None, stg: bool = False,
+                   **kwargs):
+        if client:
+            return client, CargoClient(client)
         suffix = 'io' if stg else 'com'
         wiki = EsportsClient.get_wiki(wiki)
         url = '{}.gamepedia.{}'.format(wiki, suffix)
         if self.existing_wikis[url]:
             return self.existing_wikis[wiki]['client'], self.existing_wikis[wiki]['cargo_client']
-        client = WikiClient(url, path='/', credentials=self.credentials, **self.login_args)
+        client = WikiClient(url, path='/', credentials=credentials, **kwargs)
         cargo_client = CargoClient(client)
         self.existing_wikis[url] = {'client': client, 'cargo_client': cargo_client}
         return client, cargo_client
